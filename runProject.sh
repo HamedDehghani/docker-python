b56d14f57a010992ab110c8e689be9126ec9c538
hdehghani <hamed.1de@gmail.com>
2018-09-04 09:23:57 +0430

    main

--- runProject.sh
+++ runProject.sh
@@ -4,7 +4,8 @@ echo 'running main.py ...'
 #grep -q -F '172.16.11.133 elastic02' /etc/hosts || echo '172.16.11.133 elastic02' >> /etc/hosts
 
 #/var/spool/cron/crontabs/
-crontab -l | { cat; echo "*/1 * * * * root /usr/bin/python /app/main.py"; } | crontab -
-crontab -l
+#crontab -l | { cat; echo "*/1 * * * * root /usr/bin/python /app/main.py"; } | crontab -
+#crontab -l
 pip install --upgrade setuptools
-#python main.py
+python main.py
+tail -F /dev/null
