31031cd7a46d876a902cf15fe5617f3defdb3281
hdehghani <hamed.1de@gmail.com>
2018-09-04 11:12:50 +0430

    main

--- runProject.sh
+++ runProject.sh
@@ -4,7 +4,7 @@ echo 'running main.py ...'
 #grep -q -F '172.16.11.133 elastic02' /etc/hosts || echo '172.16.11.133 elastic02' >> /etc/hosts
 
 #/var/spool/cron/crontabs/
-#crontab -l | { cat; echo "*/1 * * * * root /usr/bin/python /app/main.py"; } | crontab -
+crontab -l | { cat; echo "*/1 * * * * /usr/bin/python /app/docker-python/main.py"; } | crontab -
 #crontab -l
 pip install --upgrade setuptools
 python main.py
